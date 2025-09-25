892a6011ad20eb6af366d898b7a03f45034f33ea
bomboonsan <bomboonsan@gmail.com>
2025-09-25 15:30:03 +0700

    finish

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,12 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  // ทำให้ Strapi ทำงานที่ root path ('') เสมอ
-  // ไม่ต้องใช้ env var ใดๆ ทั้งสิ้น
-  url: '',
+  port: env.int('PORT', 1337),
+  // สำหรับ production ให้ระบุ URL ที่ชัดเจน หรือใช้ env variable
+  url: env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
