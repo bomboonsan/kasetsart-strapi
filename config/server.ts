ba2d3806d4383d23c7904eb1c623005dc9763318
bomboonsan <bomboonsan@gmail.com>
2025-09-25 14:39:57 +0700

    finish

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  url: env('URL_PATH_PREFIX', ''),
+  url: env('URL', ''), 
   app: {
     keys: env.array('APP_KEYS'),
   },
